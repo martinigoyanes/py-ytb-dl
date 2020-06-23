48763e3d1741c91826467fbd916ff156b7ed92f9
Martin Iglesias <100384068@alumnos.uc3m.es>
2020-06-23 14:50:42 +0200

    metadata added but doesnt download

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time, helper, threading, config, spotify, song as Song
 
 #* Use 1st key first time, and if we need to download more stuff change keynum to 2 so we use the second key
 config.init_globals(keynum=2)
-verbose = False
+verbose = True
 
 spoti = spotify.Spotify(config.client_id,config.client_secret)
 spoti.get_auth_code()
--- song.py
+++ song.py
@@ -3,6 +3,10 @@ import time
 import config
 import glob
 import os
+from mutagen.id3 import ID3, APIC, USLT, _util
+from mutagen.mp3 import EasyMP3
+from mutagen import File
+from urllib.request import urlopen
 
 
 
@@ -17,7 +21,8 @@ class Song:
         self.fullname = name 
         for artist in artists:
             self.fullname += f' - {artist}'
-        self.fullpath = f'/Users/martin/Desktop/{self.fullname}.%(ext)s' 
+        self.outtemplate = f'/Users/martin/Desktop/{self.fullname}.%(ext)s' 
+        self.fullpath = f'/Users/martin/Desktop/{self.fullname}.mp3' 
         self.thread = None
         self.failed = False
         self.metadata = {
@@ -40,9 +45,9 @@ class Song:
                 'preferredquality': '192',
             },
             ],
-            'outtmpl' : self.fullpath,
+            'outtmpl' : self.outtemplate,
             'retries': 10,
-            'quiet': not verbose
+            # 'quiet': not verbose
         }
 
         if self.failed is True:
@@ -67,15 +72,15 @@ class Song:
         #     url_file.write(f'{self.name} %% {self.artists} %% {album} %% {cover_url} %% {self.video_url}\n')       
 
     def thread_handler(self, song_list_len,verbose):
-        try:
-            # self.search()
-            pass
-        except Exception as e:
-            print(f'ERROR while searching for {self.fullname}. Reason -> {str(e)}\n') 
-            self.failed = True
-            with config.started_songs_lock:
-                config.started_songs -= 1
-                return
+        # try:
+        #     # self.search()
+        #     pass
+        # except Exception as e:
+        #     print(f'ERROR while searching for {self.fullname}. Reason -> {str(e)}\n') 
+        #     self.failed = True
+        #     with config.started_songs_lock:
+        #         config.started_songs -= 1
+        #         return
         try:
             self.download(song_list_len, verbose=verbose)
         except Exception as e:
@@ -109,5 +114,38 @@ class Song:
                     print(  "\"" + self.name + "\" downloaded. [" + str(config.downloaded_songs) + "/" + str(song_list_len) + "]" + " songs downloaded....")
                 else:
                     print("\"" + self.name + "\" downloaded. Finished downloading " + str(song_list_len) + " songs")
+            # self.add_metadata()
+        
+    def add_metadata(self):
+        print("Adding metadata to \"" + self.fullname + "\"")
+        #* Add album cover
+        try:
+            cover_img = urlopen(self.metadata['cover_url'])
+        except:
+            print("ERROR: Could NOT get album cover for \"" + self.fullname + "\"")
+        
+        audio = EasyMP3(self.fullpath, ID3=ID3)
 
+        try:
+            audio.add_tags()
+        except _util.error:
+            print("ERROR: Could NOT \"add_tags()\" for \"" + self.fullname + "\"")
+        
+        audio.tags.add(
+            APIC(
+                encoding=3,  # UTF-8
+                mime='image/png',
+                type=3,  # 3 is for album art
+                desc='Cover',
+                data=cover_img.read()  # Reads and adds album art
+            )
+        )
+        audio.save()
+        
+        print("Added album cover to \""+self.fullname+"\"")
+
+        #* Add album info
+        tags = EasyMP3(self.fullpath)
+        tags["album"] = self.metadata['album']
+        tags.save()
         
